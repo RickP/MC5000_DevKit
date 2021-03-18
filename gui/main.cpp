7b3ff5351dffe4212bbdb328a5be6887139bad94
rickp <pannen@gmail.com>
2021-03-18 09:38:44 +0100

    Multiarch linux build

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,4 +1,4 @@
-#include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QFontDatabase>
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
     if (QFontDatabase::addApplicationFont(":/font/Roboto-Regular.ttf") == -1) {
         qWarning() << "Failed to load Roboto-Regular.ttf";
@@ -16,11 +16,6 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
 
     SerialCommunication* serial = new SerialCommunication();
 
