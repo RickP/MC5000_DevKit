1488262d38f9e9ea453f97da7717842e7a846eb5
rickp <pannen@gmail.com>
2021-03-18 10:53:38 +0100

    Made osx notarization work with QT5.12

--- gui/imagebuilder/macos.sh
+++ gui/imagebuilder/macos.sh
@@ -6,14 +6,22 @@ mkdir build
 cd build
 qmake ..
 make
+echo "--> Start packaging process"
+macdeployqt MC5000-GUI.app -qmldir=..
 
 if [[ "${APPLE_DEV_IDENTITY}" == "" ]]; then
 	echo "WARNING: No credentials for signing found"
 	echo "WARNING: dmg package won't be signed and notarized"
-	echo "--> Start packaging process"
-	macdeployqt MC5000-GUI.app -qmldir=.. -dmg
+  echo "--> Creating dmg"
+  hdiutil create -srcfolder MC5000-GUI.app MC5000-GUI.dmg
+  mv MC5000-GUI.dmg ../images/MC5000-GUI_MacOS-x86_64-unsigned.dmg
 else
-	macdeployqt MC5000-GUI.app -qmldir=.. -dmg -hardened-runtime -codesign=${APPLE_DEV_IDENTITY}
+  echo "--> Signing app bundle"
+  codesign -vvv --deep --strict --options=runtime --sign ${APPLE_DEV_IDENTITY} MC5000-GUI.app
+  echo "--> Creating dmg"
+  hdiutil create -srcfolder MC5000-GUI.app MC5000-GUI.dmg
+  echo "--> Signing dmg"
+  codesign -vvv --strict --options=runtime --sign ${APPLE_DEV_IDENTITY} MC5000-GUI.dmg
 	response=$(xcrun altool -t osx -f MC5000-GUI.dmg --primary-bundle-id "org.rickp.mc5000" --notarize-app -u "${APPLE_DEV_USER}" -p "${APPLE_DEV_PASS}")
 	requestUUID=$(echo "${response}" | tr ' ' '\n' | tail -1)
 	for ((ATTEMPT=5; ATTEMPT>=1; ATTEMPT--))
@@ -33,12 +41,13 @@ else
 	      exit ${EXIT_CODE}
 	    fi
 	    echo "Stapler done!"
+      mv MC5000-GUI.dmg ../images/MC5000-GUI_MacOS-x86_64.dmg
 	    break
 	  fi
 	  if [[ "${isFailure}" != "" ]]; then
 	    echo "${statusCheckResponse}"
 	    echo "Notarization failed"
-	    exit 1
+      break
 	  fi
 
 	  echo "Notarization not finished yet, sleep 2m then check again..."
@@ -49,6 +58,6 @@ else
 	  done
 	done
 fi
-mv MC500-GUI.dmg ../images/MC500-GUI_MacOS-x86_64.dmg
-cd ../imagebuilder
-rm -r ../build
+
+cd ..
+rm -r build
