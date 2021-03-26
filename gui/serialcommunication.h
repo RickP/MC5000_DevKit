2a3a2cff473d0ee1f92ee673bf010ce5b39f1090
rickp <pannen@gmail.com>
2021-03-26 01:46:31 +0100

    Upload fix

--- gui/serialcommunication.h
+++ gui/serialcommunication.h
@@ -21,8 +21,8 @@ const QString COMMENT_MARKER = "#";
 const char LABEL_HEXCODE = 0x10 << 2;
 const char START_CHAR = 0x7F;
 const char END_CHAR = 0x7E;
-const int SERIAL_DELAY = 6;
-const int UPLOAD_RETRIES = 3;
+const int SERIAL_DELAY = 8;
+const int UPLOAD_RETRIES = 10;
 const int MAX_PROGRAM_LENGTH = 74;
 
 public:
